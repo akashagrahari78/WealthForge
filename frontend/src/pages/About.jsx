6d2f8a64d9ce9d4b95ed3249606dc03a3ec5f8ae
Akash Agrahari <akashagrahari746@gmail.com>
2025-10-15 20:09:12 +0530

    mission-vision section added

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,12 +1,13 @@
 import AboutHero from "../components/About/AboutHero";
 import AboutHeroV2 from "../components/About/AboutHeroV2";
+import MissionVision from "../components/About/MissionVision";
 
 export default function About() {
   return (
     <>
       {/* <AboutHero /> */}
         <AboutHeroV2/>
-      {/* other about sections */}
+        <MissionVision/>
     </>
   );
 }
