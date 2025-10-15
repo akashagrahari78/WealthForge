5d7515a4abd32ccfda5a75513bcb03a83d6f93c7
Akash Agrahari <akashagrahari746@gmail.com>
2025-10-16 00:21:27 +0530

    about page feathure add

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,14 +2,18 @@ import AboutHero from "../components/About/AboutHero";
 import AboutHeroV2 from "../components/About/AboutHeroV2";
 import FoundersStory from "../components/About/FoundersStory";
 import MissionVision from "../components/About/MissionVision";
+import OurValues from "../components/About/OurValues";
+import Navbar from "../components/Hero/Navbar";
 
 export default function About() {
   return (
     <>
       {/* <AboutHero /> */}
+      <Navbar/>
       <AboutHeroV2 />
       <MissionVision />
       <FoundersStory />
+      <OurValues/>
     </>
   );
 }
